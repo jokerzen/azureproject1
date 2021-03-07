1df3122b2e209ec2880b086e496cc547fe31e922
cw9404 <cw9404@att.com>
2021-03-07 14:58:59 -0500

    not a comma
    note:
    _build_msal_app(authority=authority), get_authorization_request_url
    should have been:
    _build_msal_app(authority=authority).get_authorization_request_url

--- FlaskWebProject/views.py
+++ FlaskWebProject/views.py
@@ -63,7 +63,7 @@ def post(id):
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    app.logger.info('LOGIN')
+    app.logger.info('LOGIN ------------')
     if current_user.is_authenticated:
         return redirect(url_for('home'))
     form = LoginForm()
@@ -147,7 +147,7 @@ def _build_msal_app(cache=None, authority=None):
 
 def _build_auth_url(authority=None, scopes=None, state=None):
     # TODO: Return the full Auth Request URL with appropriate Redirect URI
-    return _build_msal_app(authority=authority), get_authorization_request_url(
+    return _build_msal_app(authority=authority).get_authorization_request_url(
         scopes or [],
         state=state or str(uuid.uuid4()),
         redirect_uri=url_for('authorized', _external=True, _scheme='https')
